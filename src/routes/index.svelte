6c1153c382f64dcbaf0ef95ac1d44e90f68355af
edde746 <86283021+edde746@users.noreply.github.com>
2022-02-05 14:44:54 +0100

    Download Endpoint

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+  let url;
+  let content;
+</script>
+
+<input bind:value={url} />
+<button
+  on:click={async () => {
+    content = await fetch("/download", {
+      method: "POST",
+      body: JSON.stringify({
+        url,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then((res) => res.text());
+  }}>Download</button
+>
+
+<pre>{JSON.stringify({
+    url,
+  })}</pre>
+<pre>{content}</pre>
